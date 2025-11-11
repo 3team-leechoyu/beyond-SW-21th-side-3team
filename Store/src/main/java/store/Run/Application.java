3f9a3caa19aaad70d3011a800d154bacc9c71b29
cho-yunho01 <04whdbsgh30@naver.com>
2025-11-11 18:18:13 +0900

    feat: AI기능 완성

--- Store/src/main/java/store/Run/Application.java
+++ Store/src/main/java/store/Run/Application.java
@@ -1,7 +1,9 @@
 package store.Run;
 
+import store.controller.AiManager;
+
 public class Application {
     public static void main(String[] args) {
-        System.out.println("아녕");
+       
     }
 }
